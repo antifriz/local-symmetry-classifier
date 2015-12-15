9a33ae8d7ef956bec9c961d6672a3479a035f4d8
Ivan Jurin <ivan.jurin93@gmail.com>
2015-12-15 16:25:32 +0100

    various changes

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,12 @@
-from libSym import Image
+from libSym.FeatureExtractor import FeatureExtractor
 from libSym.ImageLoader import ImageLoader
-import os
 
-iloader = ImageLoader(data_path="../data")
-print str(iloader.get_train_image_matrices())
+imageLoader = ImageLoader(data_path="../data")
 
+train_images = imageLoader.get_train_image_matrices()
+print str(train_images)
 
+feature_extractor = FeatureExtractor(7)
+features = feature_extractor.extract_features_multi(train_images, verbose=True)
 
 
