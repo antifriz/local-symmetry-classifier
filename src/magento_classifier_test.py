b868f5e3f1e832a91c7ea8f9a4a4716fa6bc6d87
Ivan Jurin <ivan.jurin93@gmail.com>
2015-12-17 13:18:40 +0100

    magento tested, 55% accuracy

--- src/magento_classifier_test.py
+++ src/magento_classifier_test.py
@@ -9,5 +9,18 @@ image_files = image_loader.get_image_files()
 mc = MagentoClassifier()
 mc.fit(image_files['train'])
 
+
+def extract_name(path):
+    return path.split('/')[-1].split('.')[0][:-1]
+
+
+i = 0
+cnt = 0
 for filename in image_files['test']:
-    print filename,'->', image_files['train'][mc.predict(filename)]
+    predicted = image_files['train'][mc.predict(filename)]
+    is_ok = extract_name(predicted) == extract_name(filename)
+    if is_ok:
+        i += 1
+    cnt += 1
+    print ('OK' if is_ok else '--'), filename, '->', predicted
+print 'Hit:',i,'Miss:',cnt,'Percentage:',str(i / float(cnt) * 100)+"%", 'Database size:',len(image_files['train'])
