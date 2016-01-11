d5e8a01a0ca1ebdb73a9b427f30a14ea2125365b
Ivan Jurin <ivan.jurin93@gmail.com>
2016-01-11 14:49:59 +0100

    added new testing environment

--- src/magento_crosstester.py
+++ src/magento_crosstester.py
@@ -4,4 +4,4 @@ from libSym.magento import *
 data_path = '../data'
 image_loader = ImageLoader2(data_path=data_path)
 buildings = image_loader.get_buildings()
-MagentoClassifier.test_on_dataset(buildings,train_images_per_building=1,test_images_per_building=1,iterations=5,seed=69)
+MagentoClassifier.test_on_dataset(buildings,class_count=4,train_images_per_building=4,test_images_per_building=2,iterations=10,seed=69)
