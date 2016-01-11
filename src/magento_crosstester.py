e8e7969c54ad2a46d9fb9deedf3d7da4b6d68aae
Ivan Jurin <ivan.jurin93@gmail.com>
2016-01-11 20:41:57 +0100

    refactor

--- src/magento_crosstester.py
+++ src/magento_crosstester.py
@@ -1,7 +1,13 @@
+import itertools
+
+from libSym import magento
 from libSym.magento import *
 
 
 data_path = '../data'
 image_loader = ImageLoader2(data_path=data_path)
 buildings = image_loader.get_buildings()
-MagentoClassifier.test_on_dataset(buildings,class_count=4,train_images_per_building=4,test_images_per_building=-1,iterations=10,seed=-1)
+
+magento.LOG_LEVEL = 4
+
+MagentoClassifier.test_on_dataset(buildings,class_count=2,train_images_per_building=1,test_images_per_building=-1,iterations=1,seed=-1)
