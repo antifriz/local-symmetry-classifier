83f1cbbb829d1e85f2f16b5eeae1d080a0bb2730
Ivan Jurin <ivan.jurin93@gmail.com>
2016-01-12 17:57:03 +0100

    set seed to -1

--- src/magento_crosstester.py
+++ src/magento_crosstester.py
@@ -11,4 +11,4 @@ buildings = image_loader.get_buildings()
 magento.LOG_LEVEL = 4
 magento.CPU_COUNT = 1
 magento.SHOW_DETECTIONS = True
-MagentoClassifier.test_on_dataset(buildings,class_count=4,train_images_per_building=4,test_images_per_building=-1,iterations=100,seed=1)
+MagentoClassifier.test_on_dataset(buildings,class_count=4,train_images_per_building=4,test_images_per_building=-1,iterations=100,seed=-1)
