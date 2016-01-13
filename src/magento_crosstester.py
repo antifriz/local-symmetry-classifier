e654b48f9f81cf6fca989ec004b13a035843c985
Ivan Jurin <ivan.jurin93@gmail.com>
2016-01-13 21:37:03 +0100

    fixed descriptor bug, refactor

--- src/magento_crosstester.py
+++ src/magento_crosstester.py
@@ -1,15 +1,14 @@
-import itertools
-
 from libSym import magento
 from libSym.magento import *
 
 
-data_path = '../data'
-image_loader = ImageLoader2(data_path=data_path)
-buildings = image_loader.get_buildings()
+if __name__ == '__main__':
+    magento.DATA_PATH = '../data'
+    image_loader = ImageLoader2(data_path=magento.DATA_PATH)
+    buildings = image_loader.get_buildings()
 
-magento.LOG_LEVEL = 0
-magento.CPU_COUNT = 8
-magento.SHOW_DETECTIONS = False
+    magento.LOG_LEVEL = 0
+    magento.CPU_COUNT = 3
+    magento.SHOW_DETECTIONS = False
 
-MagentoClassifier.test_on_dataset(buildings,class_count=-1,train_images_per_building=4,test_images_per_building=-1,iterations=10,seed=-1,method='sum')
+    MagentoClassifier.test_on_dataset(buildings,class_count=-1,train_images_per_building=4,test_images_per_building=-1,iterations=3,seed=30,method='sum')
