032621d937215b462b8b6d19d187d078c518616d
Ivan Jurin <ivan.jurin93@gmail.com>
2016-01-13 15:13:35 +0100

    added multiple knn decision methods

--- src/magento_crosstester.py
+++ src/magento_crosstester.py
@@ -8,7 +8,8 @@ data_path = '../data'
 image_loader = ImageLoader2(data_path=data_path)
 buildings = image_loader.get_buildings()
 
-magento.LOG_LEVEL = 4
-magento.CPU_COUNT = 1
-magento.SHOW_DETECTIONS = True
-MagentoClassifier.test_on_dataset(buildings,class_count=4,train_images_per_building=4,test_images_per_building=-1,iterations=100,seed=-1)
+magento.LOG_LEVEL = 0
+magento.CPU_COUNT = 8
+magento.SHOW_DETECTIONS = False
+
+MagentoClassifier.test_on_dataset(buildings,class_count=-1,train_images_per_building=4,test_images_per_building=-1,iterations=10,seed=-1,method='sum')
