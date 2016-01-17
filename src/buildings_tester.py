987921eda43c8444334e50423de563fca1160cc9
Ivan Jurin <ivan.jurin93@gmail.com>
2016-01-17 12:19:19 +0100

    added certainity percentage in HIT/MISS log

--- src/buildings_tester.py
+++ src/buildings_tester.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
     # image_loader = ImageLoader(data_path=magento.DATA_PATH,haar_cascade='../faces/haarcascade_frontalface_default.xml')
     buildings = image_loader.get_buildings()
 
-    magento.LOG_LEVEL = 0
-    magento.CPU_COUNT = 1
+    magento.LOG_LEVEL = 2
+    magento.CPU_COUNT = 2
     magento.SHOW_DETECTIONS = False
-    magento.USE_CACHE = False
+    magento.USE_CACHE = True
 
-    MagentoClassifier.test_on_dataset(buildings, class_count=4, train_images_per_building=2,
-                                      test_images_per_building=-1, iterations=3, seed=30, method='sum')
+    MagentoClassifier.test_on_dataset(buildings, class_count=4, train_images_per_building=4,
+                                      test_images_per_building=-1, iterations=100, seed=-1, method='sum')
